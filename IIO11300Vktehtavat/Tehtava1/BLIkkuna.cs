afc354fd6de54e60deb8c339402d4af535f74eb2
Straikkeri <aleksi.tommila@gmail.com>
2016-01-20 11:12:06 +0200

    Tehtava 1 täydennetty. Get, Set ja propertyt.

---
+++ IIO11300Vktehtavat/Tehtava1/BLIkkuna.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.IIO11300 {
+    class IkkunaVE0 {
+
+        public double leveys, korkeus;
+        public double LaskePintaala() {
+            return leveys * korkeus;
+        }
+    }
+    public class Ikkuna {
+        //property = Ominaisuus
+        private double korkeus;
+
+        public double Korkeus {
+            get { return korkeus; }
+            set { korkeus = value; }
+        }
+
+        private double leveys;
+
+        public double Leveys {
+            get { return leveys; }
+            set { leveys = value; }
+        }
+
+        //read.only tyyppinen property
+        public double PintaAla {
+            get {
+                //return korkeus * leveys;
+                return LaskeIPintaAla();
+            }
+        }
+        //Metodit
+        public double LaskePintaAla() {
+            //return korkeus * leveys;
+            return LaskeIPintaAla();
+        }
+        private double LaskeIPintaAla() {
+            return korkeus * leveys;
+        }
+    }
+}
--- IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
@@ -23,8 +23,7 @@ namespace Tehtava1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    public partial class MainWindow : Window {
         public MainWindow()
         {
             InitializeComponent();
@@ -33,25 +32,33 @@ namespace Tehtava1
         private void btnCalculate_Click(object sender, RoutedEventArgs e)
         {
             //TODO
-            try
-            {
+            try{
                 tulosPA.Text = Convert.ToString(Double.Parse(boxWinHeight.Text) * Double.Parse(boxWinWidth.Text));
                 tulosKP.Text = Convert.ToString((Double.Parse(boxWinWidth.Text) + Double.Parse(boxSideWidth.Text)*2)*2 + Double.Parse(boxWinHeight.Text)*2);
                 tulosKPA.Text = Convert.ToString(Double.Parse(boxWinHeight.Text) * Double.Parse(boxSideWidth.Text) + ((Double.Parse(boxWinWidth.Text) + Double.Parse(boxSideWidth.Text) * 2) * Double.Parse(boxSideWidth.Text)) * 2);
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex){
                MessageBox.Show(ex.Message);
             }
-            finally
-            {
+            finally{
                 //yield to a user that everything okay
-            }
+            }  
         }
 
-        private void btnClose_Click(object sender, RoutedEventArgs e)
-        {
+        private void btnClose_Click(object sender, RoutedEventArgs e){
 
         }
+
+        private void btnCalculateArea00_Click(object sender, RoutedEventArgs e) {
+            JAMK.IT.IIO11300.Ikkuna ikk = new JAMK.IT.IIO11300.Ikkuna();
+            ikk.Korkeus = double.Parse(boxWinHeight.Text);
+            ikk.Leveys = double.Parse(boxWinWidth.Text);
+            //MessageBox.Show(ikk.LaskePintaAla().ToString());
+            MessageBox.Show(ikk.PintaAla());
+        }
+    }
+
+    public class BusinessLogicWindow {
+            
     }
 }
