a63aeba8bf2741a3835408c1c6d362a8ae0c5816
Straikkeri <aleksi.tommila@gmail.com>
2016-02-03 11:17:11 +0200

    Päiviteltiin mittausdata tehtävää. Serialisointi ja tiedostoon luku  & kirjoitus.

--- IIO11300Vktehtavat/harjoitus_03_mittausdata/BLMittaus.cs
+++ IIO11300Vktehtavat/harjoitus_03_mittausdata/BLMittaus.cs
@@ -17,6 +17,9 @@ namespace JAMK.IT.IIO11300 {
             get { return mittaus; }
             set { mittaus = value; }
         }
+        public string DataMuoto {
+            get { return kello + ";" + mittaus; }
+        }
         #region CONSTRUCTORS
         //luokalle tehdään kaksi konstruktoria
         public MittausData() {
@@ -34,5 +37,66 @@ namespace JAMK.IT.IIO11300 {
             //return base.ToString();
             return kello + "=" + mittaus;
         }
+        #region METHODS
+        public static void SaveDataToFile(List<MittausData> dataa, string filu) {
+            //kirjoitetaan data tiedostoon, jos tiedosto on jo  olemassa liitetään se olemassa olevaan
+            try {
+                //tutkitaan onko tiedosto olemassa
+                if (!System.IO.File.Exists(filu)) {
+                    //luodaan uusi
+                    using (System.IO.StreamWriter sw = System.IO.File.CreateText(filu)) {
+                        //käydään kokoelma läpi ja kirjoitetaan kukin mittausdata omalle riville
+                        foreach (var item in dataa) {
+                            sw.WriteLine(item.DataMuoto);
+                        }
+                    }
+                }
+                else {
+                    //lisätään olemassaolevaan tiedostoon
+                    using ( System.IO.StreamWriter sw = System.IO.File.AppendText(filu)) {
+                        foreach (var item in dataa) {
+                            sw.WriteLine(item.DataMuoto);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) {
+
+                throw ex;
+            }
+        }
+
+        public static List<MittausData> ReadDataFromFile(string filu) {
+            //luetaan käyttäjän antamasta tiedostosta tekstirivejä ja muutetaan ne mittausdataksi
+            try {
+                if (System.IO.File.Exists(filu)) {
+                    using(System.IO.StreamReader sr = System.IO.File.OpenText(filu)) {
+                        
+                        MittausData md;
+                        List<MittausData> luetut = new List<MittausData>();
+                        string rivi = "";
+                        while((rivi = sr.ReadLine()) != null) {
+                            //tutkitaan löytyykö sovittu erotinmerkki ; --> etupuolella on kellonaika ja jälkipuolella mittausarvo
+                            if ((rivi.Length > 3) && rivi.Contains(";")) {
+                                string[] split = rivi.Split(new char[] { ';' });
+                                //luodaan tekstinpätistä olio
+                                md = new MittausData(split[0], split[1]);
+                                luetut.Add(md);
+                            }
+                        }
+                        //palautetaan
+                        return luetut;
+                    }
+                }
+                else {
+                    throw new System.IO.FileNotFoundException();
+                }
+            }
+            catch (Exception ex) {
+
+                throw ex;
+            }
+        }
+        #endregion
     }
 }
--- IIO11300Vktehtavat/harjoitus_03_mittausdata/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/harjoitus_03_mittausdata/MainWindow.xaml.cs
@@ -19,17 +19,57 @@ namespace harjoitus_03_mittausdata {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        List<MittausData> mitatut;
         public MainWindow() {
             InitializeComponent();
             Initialize();
         }
         private void Initialize() {
             txtToday.Text = DateTime.Today.ToShortDateString();
+            mitatut = new List<MittausData>();
         }
 
         private void btnSaveData_Click(object sender, RoutedEventArgs e) {
             MittausData md = new MittausData(txtClock.Text, txtData.Text);
-            lbData.Items.Add(md);
+            //lbData.Items.Add(md); //alkuperäinen tapa
+            //lisätään mittaus-olio kokoelmaan
+            mitatut.Add(md);
+            ApplyChanges();
+        }
+        private void ApplyChanges() {
+            //päivitetään UI vastaamaan kokoelman tietoja
+            lbData.ItemsSource = null;
+            lbData.ItemsSource = mitatut;
+        }
+
+        private void btnSaveToFile_Click(object sender, RoutedEventArgs e) {
+            //TODO kutsu BL:n tallennusmetodia
+            try {
+                MittausData.SaveDataToFile(mitatut, txtFileName.Text);
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnGetFromFile_Click(object sender, RoutedEventArgs e) {
+            //luetaan datat käyttäjän antamasta tiedostosta
+            try {
+                mitatut = null;
+                mitatut = MittausData.ReadDataFromFile(txtFileName.Text);
+                ApplyChanges();
+                MessageBox.Show("Tiedot luettu onnistuneesti tiedostosta " + txtFileName.Text);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSaveToXML_Click(object sender, RoutedEventArgs e) {
+            //serialisoidaan XML:ksi
+            JAMK.IT.IIO11300.Serialisointi.SerialisoiXml(@"d:\testi.xml", mitatut);
         }
     }
 }
+
